205bcf6a68ba49fed30f5aa933ddb7d6013ca0b4
Paolo Benettin <paolo.benettin@epfl.ch>
2019-09-12 15:38:20 +0200

    avoid re-plotting measurements

--- plot_results.m
+++ plot_results.m
@@ -28,19 +28,21 @@ if flag_plot.Cout==1
     figure(101)
     hold on
     
-    % plot the measured and modeled timeseries
-    if ~exist('p1','var') %avoid replotting the data if it is already in the plot
+    % plot the measured timeseries
+    tmp=get(gcf,'Children'); tmp=get(tmp,'Children');
+    if isempty(tmp) %only add the plot if not done before
         stairs(data.dates(data.C_J~=0),data.C_J(data.C_J~=0),'-','MarkerSize',3,...
             'DisplayName','input','Color',[.8 .8 .8])
-        p1=plot(data.dates(data.indexC_Q),data.measC_Q,'og','DisplayName','measured',...
+        pm=plot(data.dates(data.indexC_Q),data.measC_Q,'-og','DisplayName','measured',...
             'MarkerSize',3,'MarkerEdgeColor',[.5 1 .5],'MarkerFaceColor','g');
     end
     
     % plot the modeled timeseries
-    plot(data.dates,C_Q,'-','DisplayName','modeled (continuous)')
-    plot(data.dates(data.indexC_Q),C_Q(data.indexC_Q),'o','DisplayName',...
-        'modeled (during measurements)',...
-       'MarkerSize',3,'MarkerEdgeColor','k')
+    pmod=plot(data.dates,C_Q,'-','DisplayName','modeled (continuous)');
+    col=get(pmod,'Color');
+    plot(data.dates(data.indexC_Q),C_Q(data.indexC_Q),'o','MarkerEdgeColor',col,...
+        'DisplayName','modeled (during measurements)',...
+       'MarkerSize',3)
     
     % complete the plot
     datetick('x','mmm-yy','keepticks')
